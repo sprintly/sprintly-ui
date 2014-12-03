98829fd1cc3b7dc9a9c887fd9bca43b7360334e3
florapdx <floraworleypdx@gmail.com>
2014-12-03 10:30:34 -0800

    maybe getting a little closer to coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,153 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha-phantomjs');
-var istanbul = require('gulp-istanbul');
+'use strict';
 
-JS_SRC = './src/js/**/*.js';
-JS_MAIN = './src/js/index.js';
-JS_TESTS = './src/js/**/*_test.js';
+var gulp = require('gulp');
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
+var istanbul = require('gulp-istanbul');
+var jsx = require('gulp-jsx');
+var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+
+
+/*
+ * Dev
+ */
+
+gulp.task('build', function() {
+  var bundler = browserify('./src/js/index.js', {
+    standalone: 'SprintlyUI',
+    exclude: 'react',
+    transform: ['reactify'],
+    debug: true,
+    verbose: true
+  });
+
+  return bundler.bundle()
+    .pipe(source('sprintly-ui.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', ['build'], function() {
+  var bundler = watchify('./src/js/index.js', watchify.args);
+  bundler.transform('reactify');
+  bundler.on('update', rebundle);
+
+  function rebundle() {
+    return gulp.run('build');
+  }
+});
+
+
+/*
+ * Test
+ */
+
+
+gulp.task('build-test', function(cb) {
+  var bundler = browserify('./test/index.js', {
+    debug: true,
+    verbose: true
+  });
+
+  return bundler.bundle()
+      .pipe(source('build.js'))
+      .pipe(gulp.dest('./test/'));
+});
+
+gulp.task('watch-test', ['build-test'], function() {
+  var bundler = watchify('./test/build.js', watchify.args);
+  bundler.transform('reactify');
+  bundler.on('update', rebundle);
+
+  function rebundle() {
+    gulp.run('build-test');
+  }
+});
 
 gulp.task('test', function() {
-  return gulp.src('./test/index.html')
-    .pipe(mocha({
+  var runner = gulp.src('./test/index.html')
+    .pipe(mochaPhantomJS({
       reporter: 'dot'
     }));
 });
 
-gulp.task('test-server', function() {
-  connect.server({
-    port: 8888,
-    middleware: function(connect, opts) {
-      return [
-        ///....
-      ]
-    }
-  });
-  /// run phantom tests, etc
-  connect.serverClose();
+gulp.task('instrument', function() {
+  return gulp.src(['./src/js/**/**/*.js'])
+    .pipe(jsx())
+    .pipe(istanbul({
+      coverageVariable: '__coverage__'
+    }))
+    .pipe(gulp.dest('./test/tmp/'));
 });
 
-gulp.task('istanbul', function(cb) {
-  gulp.src(['./dist/sprintly-uikit.js'])
-    .pipe(istanbul())
+gulp.task('test-coverage', ['instrument'], function(cb) {
+  gulp.src('./test/index.html')
+    .pipe(mochaPhantomJS({
+      path: 'file:///Users/floraworley/Projects/sprintly_ui/sprintly-ui/test/index.html',
+      phantomjs: {
+        hooks: '/Users/floraworley/Projects/sprintly_ui/sprintly-ui/test/phantom_hooks'
+      },
+    }))
+    .pipe(istanbul.writeReports())
     .on('end', cb);
 });
 
-gulp.task('test-coverage', function() {
-  gulp.run('istanbul', function() {
-    gulp.src(['./test/build.js'])
-      .pipe(mocha())
-      .pipe(istanbul.writeReports({
-        dir: './coverage',
-        reporters: ['lcov'],
-        reportOpts: {dir: './coverage'}
-      }));
-  });
-});
+// gulp.src('./test/build.js')
+//     .pipe(sourcemaps.init())
+//     .pipe(sourcemaps.write();
 
+// gulp.task('test-coverage', ['instrument'], function() {
+//   var page = require('webpage').create();
+//   var coverage = gulp.run('test')
+//     .pipe(page.evaluate(function() {
+//       return window.__coverage__.toJSON();
+//     }))
+//     .pipe(source('coverage.json'))
+//     .pipe(gulp.dest('./test/'));
+
+//   return coverage;
+// });
+
+// gulp.task('test-server', function() {
+//   connect.server({
+//     port: 9000,
+//     middleware: function(connect, opts) {
+//       return [
+//         ///....
+//       ]
+//     }
+//   });
+//   //run phantom tests
+//   connect.serverClose();
+// });
+
+// gulp.task('browser-coverage', function() {
+//   page.open('https://local.', function() {
+//     page.evaluate(function() {
+//       return window.coverage;
+//     });
+//     phantom.exit();
+//   });
+// });
+
+// gulp.task('istanbul', function(cb) {
+//   gulp.src(['./build/test/test.js'])
+//     .pipe(istanbul())
+//     .on('end', cb);
+// });
+
+// gulp.task('test-coverage', function() {
+//   gulp.run('istanbul', function() {
+//     gulp.src(['./build/test.js'])
+//       .pipe(mocha())
+//       .pipe(istanbul.writeReports({
+//         dir: './coverage',
+//         reporters: ['lcov'],
+//         reportOpts: {dir: './coverage'}
+//       }));
+//   });
+// });
 
 
 
---
+++ test/phantom_hooks.js
@@ -0,0 +1,15 @@
+// Hook writes istanbul coverage data to coverage.json file
+module.exports = {
+  afterEnd: function(runner) {
+    var fs = require('fs');
+    var coverage = runner.page.evaluate(function() {
+      return window.__coverage__;
+    });
+    if (coverage) {
+      console.log('Writing coverage to coverage/coverage.json');
+      fs.write('coverage/coverage.json', JSON.stringify(coverage), 'w');
+    } else {
+      console.log('No coverage data generated');
+    }
+  }
+};
